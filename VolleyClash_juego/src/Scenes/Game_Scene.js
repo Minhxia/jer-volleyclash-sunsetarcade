29076c773b90109372a219b7b6b68cbe81bfd6db
Alvaro-Ibanez <alvaroibanez2705@gmail.com>
2025-11-30 17:02:05 +0100

    escenas pause y victoria

--- VolleyClash_juego/src/Scenes/Game_Scene.js
+++ VolleyClash_juego/src/Scenes/Game_Scene.js
@@ -7,6 +7,11 @@ export class Game_Scene extends Phaser.Scene {
     }
 
     create() { 
+        
+         this.input.keyboard.on("keydown-ESC", () => {
+        this.scene.pause();               // detiene el game loop
+        this.scene.launch("Pause_Scene"); // muestra la escena de pausa
+    });
 
     }
 }
