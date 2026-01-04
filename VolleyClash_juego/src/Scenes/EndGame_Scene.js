daf231a0d86044f7ba87a82cb68e47d68ed7cb1d
cchrusta <c.nioka.2022@alumnos.urjc.es>
2026-01-04 21:13:30 +0100

    Animacionez de victoria final

--- VolleyClash_juego/src/Scenes/EndGame_Scene.js
+++ VolleyClash_juego/src/Scenes/EndGame_Scene.js
@@ -20,14 +20,31 @@ export class EndGame_Scene extends Phaser.Scene {
 
         // sonido
         this.load.audio('sonidoClick', 'ASSETS/SONIDO/SonidoBoton.mp3');
+
+        //Animaciones de victoria
+        this.load.spritesheet(
+            'victoria_A', 
+            'ASSETS/PERSONAJES/ANIMACIONES/PERSONAJES_A/A_VICTORIA.png', 
+            {frameWidth:128,frameHeight: 128});
+        this.load.spritesheet(
+            'victoria_B', 
+            'ASSETS/PERSONAJES/ANIMACIONES/PERSONAJES_B/A_VICTORIA.png', 
+            {frameWidth:128,frameHeight: 128});
+        this.load.spritesheet(
+            'victoria_C', 
+            'ASSETS/PERSONAJES/ANIMACIONES/PERSONAJES_C/A_VICTORIA.png', 
+            {frameWidth:128,frameHeight: 128});
     }
 
     create() { 
+        this._createAnimations();
         const { width, height } = this.scale;
         const style = this.game.globals.defaultTextStyle;
 
         const charP1 = this.player1.name;
+        const spriteP1 = this.player1.character;
         const charP2 = this.player2.name;
+        const spriteP2 = this.player2.character;
 
         // Fondo
         this.add.image(0, 0, 'fondo').setOrigin(0).setDepth(-1);
@@ -55,10 +72,25 @@ export class EndGame_Scene extends Phaser.Scene {
             fontSize: '32px',
             color: '#5f0000ff'
         }).setOrigin(0.5);
+        let winnerCharacter;
+
+        if (this.winner === "player1") {
+            winnerCharacter = spriteP1; // 'A', 'B' o 'C'
+        } else {
+            winnerCharacter = spriteP2;
+        }
+        const animKey = `victoria_${winnerCharacter}`;
 
         // Huecos para sprites de ganador y perdedor (animaciones futuras)
         this.winnerSpriteSlot = this.add.container(width / 2 - spriteOffsetX, spriteRowY);
-        this.loserSpriteSlot = this.add.container(width / 2 + spriteOffsetX, spriteRowY);
+
+        const winnerSprite = this.add.sprite(0, 0, `victoria_${winnerCharacter}`);
+        winnerSprite.setScale(0.5);
+        winnerSprite.play(animKey);
+
+        // Añadir al contenedor del ganador
+        this.winnerSpriteSlot.add(winnerSprite);
+        
 
 
         // botón para volver al menú principal
@@ -76,6 +108,28 @@ export class EndGame_Scene extends Phaser.Scene {
             },
             textStyle: buttonTextStyle,
             clickSoundKey: 'sonidoClick',
+        });
+    }
+    _createAnimations(){
+        this.anims.create({
+            key: 'charA_victory',
+            frames: this.anims.generateFrameNumbers('victoria_A', { start: 0, end: 1 }),
+            frameRate: 4,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'charB_victory',
+            frames: this.anims.generateFrameNumbers('victoria_B', { start: 0, end: 1 }),
+            frameRate: 4,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'charC_victory',
+            frames: this.anims.generateFrameNumbers('victoria_C', { start: 0, end: 1 }),
+            frameRate: 4,
+            repeat: -1
         });
     }
 }
